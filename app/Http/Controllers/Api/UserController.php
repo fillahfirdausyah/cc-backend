262056a297bfc6ff116fa0d94161aad99d6be864
firdausyah22 <fillahfirdausyah22@gmail.com>
2020-09-28 17:05:22 +0700

    membuat API detail user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
