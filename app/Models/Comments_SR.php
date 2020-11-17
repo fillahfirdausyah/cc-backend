f909ad2d4667fa76738281f6fbfafbdfb2f56aca
warangkaer <yusrilbagas135@gmail.com>
2020-11-17 09:44:39 +0700

    tambah Model, Controller, Views, JS, CSS, ubah routes

---
+++ app/Models/Comments_SR.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comments_SR extends Model
+{
+    use HasFactory;
+
+    protected $table = 'comments_showroom';
+    protected $fillable = ['comment', 'parent_id', 'post_id'];
+
+    public function comment(){
+    	return $this->belongsTo('App\Models\SR');
+    } 
+
+    public function user()
+    {
+    	return $this->belongsTo('App\Models\User');
+    }
+
+    public function child()
+    {
+        return $this->hasMany('App\Models\Comments_SR', 'parent_id');
+    }
+
+    public function childs()
+    {
+        return $this->belongsTo('App\Models\Comments_SR');
+    }
+}
